bd96ecb4ad77ea7f151f4133e8cb0f519f2093ee
Truls Dahl <truls.dahl@me.com>
2016-07-25 10:11:39 +0200

    Update php_wdsl_test.php

--- php_wdsl_test.php
+++ php_wdsl_test.php
@@ -10,36 +10,22 @@ Author URI: http://tyfon.no
 defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 require('nusoap/lib/nusoap.php');
 
-
 function php_wdsl_test() {
-/* Create a class for your webservice structure, in this case: Contact */
-class Contact {
-    function Contact($id, $name) 
-    {
-        $this->id = $id;
-        $this->name = $name;
-    }
-}
-
-/* Initialize webservice with your WSDL */
-$client = new SoapClient("http://localhost:10139/Service1.asmx?wsdl");
-
-/* Fill your Contact Object */
-$contact = new Contact(100, "John");
-
-/* Set your parameters for the request */
-$params = array(
-  "Contact" => $contact,
-  "description" => "Barrel of Oil",
-  "amount" => 500,
-);
-
-/* Invoke webservice method with your parameters, in this case: Function1 */
-$response = $client->__soapCall("Function1", array($params));
-
-/* Print webservice response */
-var_dump($response);
+/*   phpinfo();*/
+/*   return "Hello world!"; */
+ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
+$client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl");
+$return = $client->hentTjenestebeskrivelser(100);
+print_r($return);
 }
 add_shortcode( 'phpwdsltest', 'php_wdsl_test' );
 
-?>
+function php_wdsl_test2() {
+/*   phpinfo();*/
+/*   return "Hello world!"; */
+ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
+$client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl");
+
+
+}
+add_shortcode( 'phpwdsltest2', 'php_wdsl_test2' );
