6415cffe4ef580c093b06199df4d46222e666e1e
Truls Dahl <truls.dahl@me.com>
2016-07-25 10:20:24 +0200

    Update php_wdsl_test.php

--- php_wdsl_test.php
+++ php_wdsl_test.php
@@ -20,12 +20,31 @@ print_r($return);
 }
 add_shortcode( 'phpwdsltest', 'php_wdsl_test' );
 
+
+
+
 function php_wdsl_test2() {
-/*   phpinfo();*/
-/*   return "Hello world!"; */
+  /* Create a class for your webservice structure, in this case: Contact */
+class Beskrivelse {
+    function Beskrivelse($id, $name) 
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
+
 ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
+
+/* Initialize webservice with your WSDL */
 $client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl");
 
+
+
+/* Invoke webservice method with your parameters, in this case: Function1 */
+$response = $client->__soapCall();
+
+/* Print webservice response */
+var_dump($response);
 
 }
 add_shortcode( 'phpwdsltest2', 'php_wdsl_test2' );
