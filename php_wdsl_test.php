307fa8a3e50caa17556c7c7524c1e6812a203f44
Truls Dahl <truls.dahl@me.com>
2016-07-25 19:25:30 +0200

    Update php_wdsl_test.php

--- php_wdsl_test.php
+++ php_wdsl_test.php
@@ -16,6 +16,14 @@ function php_wdsl_test2() {
 /* Initialize webservice with your WSDL */
 $client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl");
 
+$params = array (
+    "arg0" => $name
+);
+
+$response = $client->__soapCall(hentAlleTjenestebeskrivelser, array($params));
+
+var_dump($response);
+
 var_dump($client->__getFunctions()); 
 /*var_dump($client->__getTypes());*/
 
