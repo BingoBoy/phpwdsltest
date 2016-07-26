958a23192d624081625be05a4c9e5593f0fedf2c
Truls Dahl <truls.dahl@me.com>
2016-07-26 09:45:58 +0200

    Update php_wdsl_test.php

--- php_wdsl_test.php
+++ php_wdsl_test.php
@@ -7,36 +7,61 @@ Author: Truls Dahl
 Version: 0.52
 Author URI: http://tyfon.no
 */
-/*defined( 'ABSPATH' ) or die( 'No script kiddies please!' );*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
 require_once 'nusoap/lib/nusoap.php';
 
 
+
+
+
+/**
+ * php_wdsl_nusoap function.
+ *
+ * @access public
+ * @return void
+ */
 function php_wdsl_nusoap() {
-  
-/* Initialize webservice with your WSDL */
-$client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl", true);
-$proxy = $soap->getProxy();
 
-$response = $proxy=>hentAlleTjenestebeskrivelser();
+	/* Initialize webservice with your WSDL */
+	$client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl", true);
 
-echo $response;
+	$proxy = $soap->getProxy();
+
+	$response = $proxy->hentAlleTjenestebeskrivelser();
+
+	echo $response;
 
 
 }
 add_shortcode( 'phpwdslnusoap', 'php_wdsl_nusoap' );
 
+
+
+
+
+
+
+
+/**
+ * php_wdsl_test2 function.
+ *
+ * @access public
+ * @return void
+ */
 function php_wdsl_test2() {
-  
-/* Initialize webservice with your WSDL */
-$client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl", array('login' => '201128', 'password' => 'test1149'));
 
-$params = array (
-    "Tittel" => $title
-);
+	/* Initialize webservice with your WSDL */
+	$client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl", array('login' => '201128', 'password' => 'test1149'));
 
-$response = $client->__soapCall(hentAlleTjenestebeskrivelser, array($params));
+	$params = array (
+		"Tittel" => $title
+	);
 
-var_dump($response);
+	$response = $client->__soapCall(hentAlleTjenestebeskrivelser, array($params));
+
+	var_dump($response);
 
 
 }
@@ -44,23 +69,22 @@ add_shortcode( 'phpwdsltest2', 'php_wdsl_test2' );
 
 
 
-/*
-
-En funksjon som viser alle tilgjengelige funksjoner og typer
-
-*/
+/**
+ * funksjonsoversikt function.
+ * En funksjon som viser alle tilgjengelige funksjoner og typer
+ * @access public
+ * @return void
+ */
 function funksjonsoversikt() {
-  /* Initialize webservice with your WSDL */
-$client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl", array('login' => '201128', 'password' => 'test1149'));
+	/* Initialize webservice with your WSDL */
+	$client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl", array('login' => '201128', 'password' => 'test1149'));
 
-echo "FUNKSJONER
-";
-$allefunksjoner = $client->__getFunctions();
-echo str_replace(array('&lt;?php&nbsp;','?&gt;'), '', highlight_string( '<?php ' .     var_export($allefunksjoner, true) . ' ?>', true ) );
 
-echo "TYPE
-";
-$allefunksjoner = $client->__getTypes();
-echo str_replace(array('&lt;?php&nbsp;','?&gt;'), '', highlight_string( '<?php ' .     var_export($allefunksjoner, true) . ' ?>', true ) );
+	$allefunksjoner = $client->__getFunctions();
+	echo str_replace(array('&lt;?php&nbsp;','?&gt;'), '', highlight_string( '<?php ' .     var_export($allefunksjoner, true) . ' ?>', true ) );
+
+
+	$allefunksjoner = $client->__getTypes();
+	echo str_replace(array('&lt;?php&nbsp;','?&gt;'), '', highlight_string( '<?php ' .     var_export($allefunksjoner, true) . ' ?>', true ) );
 }
 add_shortcode( 'funksjonsoversikt', 'funksjonsoversikt' );
