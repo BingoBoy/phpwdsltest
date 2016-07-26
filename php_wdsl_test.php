605bf5d704d6c6ba497e59628764a5639fa2a733
Truls Dahl <truls.dahl@me.com>
2016-07-26 08:18:02 +0200

    Update php_wdsl_test.php

--- php_wdsl_test.php
+++ php_wdsl_test.php
@@ -28,23 +28,25 @@ var_dump($response);
 }
 add_shortcode( 'phpwdsltest2', 'php_wdsl_test2' );
 
+
+
 /*
+
 En funksjon som viser alle tilgjengelige funksjoner og typer
+
 */
 function funksjonsoversikt() {
   /* Initialize webservice with your WSDL */
 $client = new SoapClient("http://www.nasjonaltjenestekatalog.no/ws7/katalog?wsdl", array('login' => '201128', 'password' => 'test1149'));
 
+echo "FUNKSJONER
+";
 $allefunksjoner = $client->__getFunctions();
 echo str_replace(array('&lt;?php&nbsp;','?&gt;'), '', highlight_string( '<?php ' .     var_export($allefunksjoner, true) . ' ?>', true ) );
 
-echo "FUNKSJONER
-";
-
-print_r($client->__getFunctions()); 
 echo "TYPE
 ";
-
-print_r($client->__getTypes());
+$allefunksjoner = $client->__getTypes();
+echo str_replace(array('&lt;?php&nbsp;','?&gt;'), '', highlight_string( '<?php ' .     var_export($allefunksjoner, true) . ' ?>', true ) );
 }
 add_shortcode( 'funksjonsoversikt', 'funksjonsoversikt' );
